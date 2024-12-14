89ab50bf5d634f43ee4bb0d81d0e5b30a2141bcf
itsdevcoffee <itstexmex@gmail.com>
2024-12-14 03:33:38 -0500

    Changed maskkiller theme back to transparent theme

--- lua/plugins/sonokai.lua
+++ lua/plugins/sonokai.lua
@@ -1,11 +1,11 @@
-  return {
-    'sainnhe/sonokai',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.sonokai_enable_italic = true
-      vim.g.sonokai_better_performance = true
-      vim.g.sonokai_style = 'espresso' -- espresso, shusia, maia, andromeda, anlantis
-      vim.g.sonokai_transparent_background = 0
-    end
-  }
+return {
+	"sainnhe/sonokai",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.g.sonokai_enable_italic = true
+		vim.g.sonokai_better_performance = true
+		vim.g.sonokai_style = "shusia" -- espresso, shusia, maia, andromeda, atlantis
+		vim.g.sonokai_transparent_background = 2
+	end,
+}
