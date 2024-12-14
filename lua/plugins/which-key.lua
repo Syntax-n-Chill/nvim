31ed7c4fc789acda55bb44eab4fe7e9bfd67b01c
itsdevcoffee <itstexmex@gmail.com>
2024-12-14 13:19:02 -0500

    Added <leader>A to force quit without saving

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,49 +1,54 @@
 local M = {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {},
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {},
 }
 
 M.keys = {
-  {
-    "<leader>a",
-    "<cmd>quitall<CR>",
-    desc = "Force quit project"
-  },
-  {
-    "<leader>q",
-    ":q<CR>",
-    -- ":bdelete<CR>",
-    desc = "Close current window",
-    mode = { "n", "v" }
-  },
-  {
-    "<leader>w",
-    function()
-      require('bufdelete').bufdelete(0, false)
-    end,
-    desc = "Close current bufferline",
-    mode = { "n", "v" }
-  },
-  {
-    "<leader>?",
-    function()
-      require("which-key").show({ global = false })
-    end,
-    desc = "Buffer Local Keymaps (which-key)",
-  },
-  {
-    "<leader>/",
-    "<Plug>(comment_toggle_linewise_current)",
-    desc = "Comment toggle current line",
-    mode = { "n" }
-  },
-  {
-    "<leader>/",
-    "<Plug>(comment_toggle_linewise_visual)",
-    desc = "Comment toggle linewise (visual)",
-    mode = { "v" }
-  },
+	{
+		"<leader>a",
+		"<cmd>quitall<CR>",
+		desc = "Force quit project",
+	},
+	{
+		"<leader>A",
+		"<cmd>quitall!<CR>",
+		desc = "Force quit project",
+	},
+	{
+		"<leader>q",
+		":q<CR>",
+		-- ":bdelete<CR>",
+		desc = "Close current window",
+		mode = { "n", "v" },
+	},
+	{
+		"<leader>w",
+		function()
+			require("bufdelete").bufdelete(0, false)
+		end,
+		desc = "Close current bufferline",
+		mode = { "n", "v" },
+	},
+	{
+		"<leader>?",
+		function()
+			require("which-key").show({ global = false })
+		end,
+		desc = "Buffer Local Keymaps (which-key)",
+	},
+	{
+		"<leader>/",
+		"<Plug>(comment_toggle_linewise_current)",
+		desc = "Comment toggle current line",
+		mode = { "n" },
+	},
+	{
+		"<leader>/",
+		"<Plug>(comment_toggle_linewise_visual)",
+		desc = "Comment toggle linewise (visual)",
+		mode = { "v" },
+	},
 }
 
 return M
